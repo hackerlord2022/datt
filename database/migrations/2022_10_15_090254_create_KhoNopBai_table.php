01048e4eb44164884f077957a87922df20242377
hacklord2022 <huydnmpc01663@fpt.edu.vn>
2022-10-27 18:29:04 +0700

    EditDB

--- database/migrations/2022_10_15_090254_create_KhoNopBai_table.php
+++ database/migrations/2022_10_15_090254_create_KhoNopBai_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('archives_code');
             $table->string('archives_name');
             $table->date('deadline');
+            $table->text('note');
             $table->string('class_code');
             $table->rememberToken();
             $table->timestamps();
