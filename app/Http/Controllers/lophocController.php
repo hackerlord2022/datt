ef4532e3053c638a34b8df8d8846f487b6030fbd
lockoi <locntpc01840@fpt.edu.vn>
2022-10-27 20:57:55 +0700

    loc

--- app/Http/Controllers/lophocController.php
+++ app/Http/Controllers/lophocController.php
@@ -2,43 +2,43 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
-use App\Models\Semester as hk;
+use App\Models\Subject as mh;
 use App\Models\Classes as lh;
 use App\Models\User as user;
 class lophocController extends Controller
 {
     function them(){
-        $hocky =hk::all();
+        $monhoc =mh::all();
         $user =user::all();
-        return view('admin.page.ql_lop.them',['hocky'=>$hocky,'user'=>$user]);
+        return view('admin.page.ql_lop.them',['monhoc'=>$monhoc,'user'=>$user]);
     }
     function them_(){
         $lophoc = new lh ;
         $lophoc->class_code = $_POST['malh'];
         $lophoc->class_name = $_POST['tenlh'];
-        $lophoc->semester_code = $_POST['hocky'];
+        $lophoc->subject_code = $_POST['monhoc'];
         $lophoc->teacher_code = $_POST['user'];
         $lophoc->save();
         return redirect('admin/lophoc/them')->with('thongbao','Thêm Thành Công');
     }
     function sua($id){
         $lophoc =lh::find($id);
-        $hocky =hk::all();
+        $monhoc =mh::all();
         $user =user::all();
         if ($lophoc==null)return redirect('/thongbao');
-        return view('admin.page.ql_lop.sua',['hocky'=>$hocky,'lophoc'=>$lophoc,'user'=>$user]);
+        return view('admin.page.ql_lop.sua',['monhoc'=>$monhoc,'lophoc'=>$lophoc,'user'=>$user]);
     }
     function sua_($id){
         $lophoc =lh::find($id);
         $lophoc->class_code = $_POST['malh'];
         $lophoc->class_name = $_POST['tenlh'];
-        $lophoc->semester_code = $_POST['hocky'];
+        $lophoc->subject_code = $_POST['monhoc'];
         $lophoc->teacher_code = $_POST['user'];
         $lophoc->save();
         return redirect('admin/lophoc/sua/'.$lophoc->id)->with('thongbao','Sửa Thành Công');
     }
     function ds(){
-        $class = lh::leftJoin('users', 'users.id', '=', 'class.teacher_code')->get();       
+        $class = lh::leftJoin('users', 'users.id', '=', 'class.teacher_code')->get();
         $user =user::all();
         return view('admin.page.ql_lop.danhsach',['class'=>$class,'user'=>$user]);
     }
