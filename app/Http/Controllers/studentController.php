f59d86c7baddf77eda1a214b574e14f289362562
Thinh <thjh34316@gmail.com>
2022-10-28 18:49:03 +0700

    thinh2321333

--- app/Http/Controllers/studentController.php
+++ app/Http/Controllers/studentController.php
@@ -1,9 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\User;
+use App\Models\Class_Lop;
+use App\Models\ClassStudent_;
 
+use Illuminate\Hashing\BcryptHasher;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 class studentController extends Controller
 {
     //
@@ -13,11 +18,33 @@ class studentController extends Controller
 
 
     function account(){
-        
+        $User = Auth()->User()->id;
         return view("student.page.account");
     }
-    function myclass(){
-        return view("student.page.myclass");
+    function account_update(Request $request, $id){
+        $data = User::find(Auth()->User()->id);
+         
+            $data->name = $_POST['name'];
+            $data->email = $_POST['email'];
+            $data->passwword = $_POST['new_password'];
+            $data->password = (new BcryptHasher)->make($request->get('new_password'));
+            $data->save();
+
+      
+        $alert = 'Cập nhật thông tin thành công!';
+        return redirect()->back()->with('alert',$alert);
+        return view("student.page.account");
+    }
+    function myclass(){      
+      
+        $Class = DB::table('class_students')
+        ->join('class', 'class_students.class_code', '=', 'class.class_code')
+        ->select('class_students.*', 'class.class_name')
+        ->where('user_code', '=', (Auth()->User()->id))
+        ->get();
+        
+
+        return view("student.page.myclass", ['Class' => $Class]);
     }
     function reupload(){
         return view("student.page.reupload");
