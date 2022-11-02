dbd23dadc6aa93a4868b75d483c61803e9d73a62
lockoi <locntpc01840@fpt.edu.vn>
2022-11-02 16:44:29 +0700

    thongbao

--- public/asset/js/js.js
+++ public/asset/js/js.js
@@ -1,7 +1,5 @@
-jQuery(document).ready(function(){
-    jQuery('.toast__close').click(function(e){
-      e.preventDefault();
-      var parent = $(this).parent('.toast');
-      parent.fadeOut("slow", function() { $(this).remove(); } );
-    });
-  });
+
+
+
+
+
